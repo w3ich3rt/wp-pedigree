461aa70d8cfa53beaedd9bb4daeeab07b7a7a32a
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-14 14:21:32 +0100

    try

--- includes/wpped-admin.php
+++ includes/wpped-admin.php
@@ -1,6 +1,6 @@
 <div class="wrap">
     <h1 class="warnung">
-        <span><img alt="Little image of a dog" src="../public/images/dog.svg"/>Dog Pedigree Page</span></h1>
+        <span><img alt="Little image of a dog" src=<?php include_once(plugins_url('public/images/dog.svg' . __FILE__)) ?>/>Dog Pedigree Page</span></h1>
     <p class="warnung">On this page you can add dog pedigrees for your dog.</p>
     <p>Then you will have the ID to use them on your page.</p>
 </div>
