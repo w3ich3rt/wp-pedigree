5e14771715139a590b454713fa0862016607c779
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-14 12:54:38 +0100

    some changes

--- wp-pedigree.php
+++ wp-pedigree.php
@@ -11,7 +11,7 @@
      * Plugin Name:       WP Pedigree
      * Plugin URI:        https://www.wazishen.com/wp-pedigree
      * Description:       This is a plugin to create pedigree charts for animals or other things.
-     * Version:           0.0.1
+     * Version:           0.0.2
      * Requires at least: 5.2
      * Requires PHP:      7.2
      * Author:            Ullrich Weichert - w3ich3rt
@@ -25,12 +25,3 @@
 
     // Include mfp-functions.php, use require_once to stop the script if mfp-functions.php is not found
     require_once plugin_dir_path(__FILE__) . 'includes/wpped-functions.php';
-
-    // Add the stylesheet for the plugin
-    add_action('wp_enqueue_scripts', 'prefix_add_my_stylesheet');
-    function prefix_add_my_stylesheet()
-    {
-        wp_register_style('prefix-style', plugins_url('public/css/wp-pedigree-style.css', __FILE__));
-        wp_enqueue_style('prefix-style');
-    }
-
