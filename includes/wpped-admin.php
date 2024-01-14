3317eb45e20f7ba877d41562e27876a50ae14da8
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-14 14:18:53 +0100

    try and error

--- includes/wpped-admin.php
+++ includes/wpped-admin.php
@@ -1,4 +1,6 @@
 <div class="wrap">
-    <h2>Custom Menu Page</h2>
-    <p>Welcome to your custom menu page!</p>
+    <h1 class="warnung">
+        <span><img alt="Little image of a dog" src="../public/images/dog.svg"/>Dog Pedigree Page</span></h1>
+    <p class="warnung">On this page you can add dog pedigrees for your dog.</p>
+    <p>Then you will have the ID to use them on your page.</p>
 </div>
