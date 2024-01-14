49cff7950f885a470246aa1b9be5001cf8ae53c5
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-14 19:31:05 +0100

    try to get let the dogs out.

--- includes/wp-dog-pedigree-admin.php
+++ includes/wp-dog-pedigree-admin.php
@@ -1,10 +1,13 @@
 <div class="wrap">
     <div>
-        <h1 class="warnung">Dog Pedigree Page</h1>
-        <p class="warnung">On this page you can add dog pedigrees for your dog.</p>
+        <h1>Dog Pedigree Page</h1>
+        <p>On this page you can add dog pedigrees for your dog.</p>
         <p>Then you will have the ID to use them on your page.</p>
     </div>
     <div>
-        
+        <?php
+            $dogs = $wpdb->get_col("SELECT * FROM $wpdb->prefix . 'dogpedigree' ORDER BY comment_count DESC")
+            echo $dogs;
+        ?>
     </div>
 </div>
