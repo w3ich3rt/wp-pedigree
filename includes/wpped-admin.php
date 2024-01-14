cc304b0ad05d0f582e8f7986aedfddb042930e2d
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-14 14:34:35 +0100

    different aproach, again

--- includes/wpped-admin.php
+++ includes/wpped-admin.php
@@ -1,7 +1,9 @@
-$img_path = '<img src = ' . plugin_dir_url( __DIR__ ) . 'assets/logo.png' . ' style="height: 100%; width: 100%; object-fit: contain" alt="logo">';
+<?php
+    $img_path = '<img src = ' . plugin_dir_url( __DIR__ ) . 'assets/logo.png' . ' style="height: 100%; width: 100%; object-fit: contain" alt="logo">';
+?>
 
 <div class="wrap">
-    <img src=$img_path alt="logo">
+    <?php $img_path ?>
     <h1 class="warnung">Dog Pedigree Page</h1>
     <p class="warnung">On this page you can add dog pedigrees for your dog.</p>
     <p>Then you will have the ID to use them on your page.</p>
