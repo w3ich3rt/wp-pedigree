83a0a5b73a746134ae052a9e830094cd2d9fd37e
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-17 21:33:51 +0100

    should be done

--- includes/wp-dog-pedigree-admin.php
+++ includes/wp-dog-pedigree-admin.php
@@ -75,19 +75,40 @@
                 global $wpdb;
                 $result = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}dogpedigree");
                 foreach ( $result as $row ) {
-                    if($row->fur_type == 1){ $furtypehtml = '<td>'.__('wp_dog_pedigree_lang_longhair','wp-dog-pedigree').'</td>';} else { $furtypehtml = '<td>'.__('wp_dog_pedigree_lang_shorthair','wp-dog-pedigree').'</td>';}
-                    if($row->gender == 0){ $genderhtml =  '<td>'.__('wp_dog_pedigree_table_male','wp-dog-pedigree'.'</td>');} else { $genderhtml = '<td>'.__('wp_dog_pedigree_table_female','wp-dog-pedigree').'</td>';}
+                    if($row->fur_type == 1){ $furtypehtml = __('wp_dog_pedigree_lang_longhair','wp-dog-pedigree');} else { $furtypehtml = __('wp_dog_pedigree_lang_shorthair','wp-dog-pedigree');}
+                    if($row->gender == 0){ $genderhtml =  __('wp_dog_pedigree_table_male','wp-dog-pedigree');} else { $genderhtml = __('wp_dog_pedigree_table_female','wp-dog-pedigree');}
+                    if($row->champion == 0){ $championhtml =  __('wp_dog_pedigree_lang_no','wp-dog-pedigree');} else { $genderhtml = __('wp_dog_pedigree_lang_yes','wp-dog-pedigree');}
+                    if($row->multi == 0){ $multihtml =  __('wp_dog_pedigree_lang_no','wp-dog-pedigree');} else { $genderhtml = __('wp_dog_pedigree_lang_yes','wp-dog-pedigree');}
+                    switch ($row->color) {
+                        case "Red":
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_red','wp-dog-pedigree');
+                            break;
+                        case "Black":
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_black','wp-dog-pedigree');
+                            break;
+                        case "Blue":
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_blue','wp-dog-pedigree');
+                            break;
+                        case "Cream":
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_cream','wp-dog-pedigree');
+                            break;
+                        case "Fawn":
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_fawn','wp-dog-pedigree');
+                            break;
+                        default:
+                            $colorhtml = __('wp_dog_pedigree_lang_furcolor_no','wp-dog-pedigree');
+                    }
                     echo "<tr>";
                     echo "<td>$row->ID</td>";
                     echo "<td>$row->name</td>";
                     echo "<td>$row->owner</td>";
                     echo "<td>$row->breeder</td>";
-                    echo $genderhtml;
-                    echo "<td>$row->color</td>";
+                    echo "<td>$genderhtml</td>";
+                    echo "<td>%colorhtml</td>";
                     echo "<td>$row->HD_value</td>";
-                    echo $furtypehtml;
-                    if($row->champion == 0){echo "<td>No</td>";} else { echo "<td>Yes</td>";}
-                    if($row->multi == 0){echo "<td>No</td>";} else { echo "<td>Yes</td>";}
+                    echo "<td>$furtypehtml</td>";
+                    echo "<td>$championhtml</td>";
+                    echo "<td>$multihtml</td>";
                     echo "<td>$row->father</td>";
                     echo "<td>$row->mother</td>";
                     $link = admin_url('admin-post.php?action=delete_pedigree&id=' . $row->ID);
