a2539dd70303fe6dac07856cdeb040e243958c96
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-21 13:35:13 +0100

    db_table update

--- includes/wp-dog-pedigree-activation-hook.php
+++ includes/wp-dog-pedigree-activation-hook.php
@@ -1,6 +1,6 @@
 <?php
 global $wp_dog_pedigree_db;
-$wp_dog_pedigree_db = '1.0.0';
+$wp_dog_pedigree_db = '1.1.0';
 
 function wp_dog_pedigree_db_install() {
 	global $wpdb;
@@ -14,6 +14,7 @@ function wp_dog_pedigree_db_install() {
 		ID mediumint(9) NOT NULL AUTO_INCREMENT,
 		name tinytext NOT NULL,
 		owner tinytext NOT NULL,
+		owner_contact tinytext,
 		breeder tinytext,
 		gender bool NOT NULL,
 		color tinytext NOT NULL,
@@ -23,13 +24,34 @@ function wp_dog_pedigree_db_install() {
 		multi bool NOT NULL,
 		father tinytext,
 		mother tinytext,
+		dog_title tinytext,
+		dog_breed_conditions tinytext,
+		dog_miss_tooth tinytext,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
 
 	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 	dbDelta( $sql );
+	update_option( 'wp_dog_pedigree_version', $wp_dog_pedigree_db );
+	wp_dog_pedigree_update_tables_when_plugin_updating();
 
 	add_option( 'wp_dog_pedigree_db', $wp_dog_pedigree_db );
+}
+
+function wp_dog_pedigree_update_tables_when_plugin_updating() {
+	global $wpdb;
+	$oldVersion = get_option( 'wp_dog_pedigree_version', '1.0.0' );
+	$newVersion = '1.1.0';
+	$table_name = $wpdb->prefix . 'dogpedigree';
+	$charset_collate = $wpdb->get_charset_collate();
+	$sql = "ALTER TABLE $table_name ADD dog_title tinytext $charset_collate;"
+	$sql .= "ALTER TABLE $table_name ADD dog_breed_conditions ttinytextext $charset_collate;"
+	$sql .= "ALTER TABLE $table_name ADD dog_miss_tooth tinytext $charset_collate;"
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+	update_option( 'wp_dog_pedigree_version', $newVersion );
+
 }
 
 function wp_dog_pedigree_data() {
@@ -48,21 +70,25 @@ function wp_dog_pedigree_data() {
 	$example_mother = 'Sbi-Wang Ayumi';
 	
 	$table_name = $wpdb->prefix . 'dogpedigree';
-	
-	$wpdb->insert(
-		$table_name,
-		array(
-			'name' => $example_dog,
-			'owner' => $example_owner,
-			'breeder' => $example_breeder,
-			'gender' => $example_gender,
-			'color' => $example_color,
-			'HD_value' => $example_HD_value,
-			'fur_type' => $example_fur_type,
-			'champion' => $example_champion,
-			'multi' => $example_multi,
-			'father' => $example_father,
-			'mother' => $example_mother
-		)
-	);
+
+	if($wpdb->get_results("SELECT * FROM $table_name WHERE name = '$example_dog'")) {
+		return true;
+	} else {
+		$wpdb->insert(
+			$table_name,
+			array(
+				'name' => $example_dog,
+				'owner' => $example_owner,
+				'breeder' => $example_breeder,
+				'gender' => $example_gender,
+				'color' => $example_color,
+				'HD_value' => $example_HD_value,
+				'fur_type' => $example_fur_type,
+				'champion' => $example_champion,
+				'multi' => $example_multi,
+				'father' => $example_father,
+				'mother' => $example_mother
+			)
+		);
+	}
 }
