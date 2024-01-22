d1259886198bc86e9dc951a2154e0d66baab1136
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2024-01-22 20:36:38 +0100

    some style

--- includes/wp-dog-pedigree-public.php
+++ includes/wp-dog-pedigree-public.php
@@ -78,7 +78,7 @@
         if($dog_data->fur_type == 0){ $furtype = '<p class="dog-pedigree-furtype">' . __('wp_dog_pedigree_lang_shorthair','wp-dog-pedigree') . '</p>';} else { $furtype = "" ;}
         if($dog_data->champion == 1){ $champion = __('wp_dog_pedigree_table_champion_short','wp-dog-pedigree');}
         if($dog_data->multi == 1){ $champion = __('wp_dog_pedigree_table_mchamp_short','wp-dog-pedigree');} else { $champion = "" ;}
-        if($dog_data->gender == 0){ $gender = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M289.8 46.8c3.7-9 12.5-14.8 22.2-14.8H424c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-33.4-33.4L321 204.2c19.5 28.4 31 62.7 31 99.8c0 97.2-78.8 176-176 176S0 401.2 0 304s78.8-176 176-176c37 0 71.4 11.4 99.8 31l52.6-52.6L295 73c-6.9-6.9-8.9-17.2-5.2-26.2zM400 80l0 0h0v0zM176 416a112 112 0 1 0 0-224 112 112 0 1 0 0 224z"/></svg>'; } else { $gender = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M80 176a112 112 0 1 1 224 0A112 112 0 1 1 80 176zM224 349.1c81.9-15 144-86.8 144-173.1C368 78.8 289.2 0 192 0S16 78.8 16 176c0 86.3 62.1 158.1 144 173.1V384H128c-17.7 0-32 14.3-32 32s14.3 32 32 32h32v32c0 17.7 14.3 32 32 32s32-14.3 32-32V448h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H224V349.1z"/></svg>'; }
+        if($dog_data->gender == 0){ $gender = '<i class="dog-male"></i>'; } else { $gender = '<i class="dog-venus"></i>'; }
         $output = '<p class="dog-pedigree-champ">'. $champion . '</p><p class="dog-pedigree-dogname">' . $gender . $dog_name . '</p><p class="dog-pedigree-color">' . $color . '</p>' . $furtype;
         return $output;
     }
